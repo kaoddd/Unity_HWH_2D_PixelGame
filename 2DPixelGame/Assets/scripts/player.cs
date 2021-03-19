653acbde9ff0d6a4c14d3a5fb724e14501203794
kaoddd <71477198+kaoddd@users.noreply.github.com>
2021-03-19 16:10:08 +0800

    123

--- 2DPixelGame/Assets/scripts/player.cs
+++ 2DPixelGame/Assets/scripts/player.cs
@@ -1,7 +1,7 @@
 ﻿
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class player : MonoBehaviour
 {
     //註解
 
@@ -16,8 +16,9 @@ public class Player : MonoBehaviour
     //布林值 bool true 是、false 否
     //字串  string
     [Header("等級")]
+    [Tooltip("這是腳色的等級")]
     public int lv = 1 ;
-    [Range(0,300)]
+    [Header("移動速度"),[Range(0, 300)]   
 }   public float speed = 10.5f;
     public bool isDead = false;
     [Tooltip("這是角色的名稱")]
