7437c7cd6bca1fe2b68865b9c2d5de6c849e5509
kaoddd <rtjv12345656@gmail.com>
2021-03-19 13:28:23 +0800

    77

--- 2DPixelGame/Assets/scripts/player.cs
+++ 2DPixelGame/Assets/scripts/player.cs
@@ -1,10 +1,24 @@
 ﻿
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
     //註解
+
     //欄位語法
-    //類型名稱
-    int lv;
-}
+    //修飾詞 類型  名稱(指定值)
+    //私人 private 不顯示(預設值)
+    //公開 public 顯示
+
+    //類型 四大類型
+    //整數 int
+    //浮點數 float
+    //布林值 bool true 是、false 否
+    //字串  string
+    [Header("等級")]
+    public int lv = 1 ;
+    [Range(0,300)]
+}   public float speed = 10.5f;
+    public bool isDead = false;
+    [Tooltip("這是角色的名稱")]
+    public string cName = "貓咪";
